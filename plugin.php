af89593fcc68fed957ad0dcb71e22707f412dfd8
curiousmaster <curious.master@gmail.com>
2024-11-25 12:58:51 +0100

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'id' =>             'osticket:socmattermost',
+    'version' =>        '1.0.0',
+    'name' =>           'SOCMattermost Integration',
+    'author' =>         'Stefan Benediktsson',
+    'description' =>    'Send notifications to Mattermost based on ticket severity.',
+    'url' =>            'https://github.com/curiousmaster/socmattermost',
+    'plugin' =>         'socmattermost.php:SOCMattermostPlugin',
+);
+
+?>
